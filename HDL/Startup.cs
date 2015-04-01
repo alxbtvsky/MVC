5e3e87a612d4ed494d8fc6c005bc980f655f17c1
alxbtvsky <alxbtvsky@gmail.com>
2015-04-01 12:50:12 -0500

    Reworked project

---
+++ HDL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HDL.Startup))]
+namespace HDL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
